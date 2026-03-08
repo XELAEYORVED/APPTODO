e85d27d1536609d26fc4a55f34e2e32db31e14c3
Alexdevr09 <alexandre.devroye@gmail.com>
2026-03-08 14:29:10 +0100

    add feature
    formulaire

--- todoapp/src/App.tsx
+++ todoapp/src/App.tsx
@@ -1,13 +1,37 @@
+type Priority = "Urgente" | "Moyenne" | "Basse"
 
+type Todo = {
+  id: number;
+  text: string;
+  priority: Priority 
+}
 
 function App() {
-
-
   return (
     
-      <div className = "">
-          <button className = "btn btn-accent">test</button>
+    <div className = "flex justify-center">
+      <div className = "w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2x1">
+        <div className ="flex gap-4">
+          <input 
+            type="text"
+            className="input w-full"
+            placeholder="ajouter une tâche..."
+           />
+           <select className="select w-full">
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+
+           </select>
+           <button className="btn btn-primary">
+            Ajouter
+           </button>
+
+        </div>
+
       </div>
+          
+    </div>
      
   )
 }
